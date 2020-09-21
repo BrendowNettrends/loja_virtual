1cf39bababc6fce4f8fff5c4bdc4d8a0115a00c5
BrendowNettrends <brendow.lincoln@nettrends.com.br>
2020-09-21 10:19:11 -0300

    Criando tile da Lista

--- loja_virtual/lib/tiles/product_tile.dart
+++ loja_virtual/lib/tiles/product_tile.dart
@@ -10,6 +10,12 @@ class ProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context)=>ProductScreen(product))
+        );
+      },
       child: Card(
         child: type == "grid" ?
             Column(
@@ -50,6 +56,41 @@ class ProductTile extends StatelessWidget {
             )
             : Row(
 
+          children: <Widget> [
+            Flexible(
+              flex: 1,
+              child: Image.network(
+                product.images[0],
+                fit: BoxFit.cover,
+                height: 250.0,
+              )
+            ),
+            Flexible(
+              flex: 1,
+              child:Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget> [
+                      Text(
+                        product.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      Text(
+                          "R\$ ${product.price.toStringAsFixed(2)}",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold,
+                          )
+                      )
+                    ],
+                  )
+              ),
+            ),
+          ],
         )
       ),
     );
