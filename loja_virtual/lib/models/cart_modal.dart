66c298fd36b7c6bff285226daa089cb322376ce2
BrendowNettrends <brendow.lincoln@nettrends.com.br>
2020-09-23 09:42:32 -0300

    Criadno o corpo da tela do carrinho

--- loja_virtual/lib/models/cart_modal.dart
+++ loja_virtual/lib/models/cart_modal.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/cart_product.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -9,6 +9,8 @@ class CartModel extends Model {
   UserModel user;
 
   List<CartProduct> products = [];
+
+  bool isLoading = false;
 
   CartModel(this.user);
 
