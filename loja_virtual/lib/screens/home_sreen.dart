78c94894166cc99b157c4134b53aff4c8454e176
BrendowNettrends <brendow.lincoln@nettrends.com.br>
2020-09-24 13:33:48 -0300

    Criando tab de pedidos

--- loja_virtual/lib/screens/home_sreen.dart
+++ loja_virtual/lib/screens/home_sreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/orders_tab.dart';
 import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widget/cart_button.dart';
 import 'package:loja_virtual/widget/custom_drawer.dart';
@@ -27,7 +28,16 @@ class HomeScreen extends StatelessWidget {
          drawer: CustomDrawer(_pageController),
          body: ProductsTab(),
          floatingActionButton: CartButton(),
-       )
+       ),
+      Container(color: Colors.yellow,),
+      Scaffold(
+        appBar: AppBar(
+          title: Text("Meus Pedidos"),
+          centerTitle: true,
+        ),
+        body: OrdersTab(),
+        drawer: CustomDrawer(_pageController),
+      )
       ],
     );
   }
